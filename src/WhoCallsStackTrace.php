20f3dc222f0a18a0a6f1d69de74d982d58227199
Matt Butcher <technosophos@gmail.com>
2010-12-07 15:30:36 -0600

    Made it pretty.

--- src/WhoCallsStackTrace.php
+++ src/WhoCallsStackTrace.php
@@ -1,24 +1,21 @@
 <?php
 class WhoCallsStackTrace extends StackTraceForSymbol {
-  public function format_internal_function($depth, $function_name, $elapsed_time, $memory_consumption) {
-    return $function_name . "\t[INTERNAL]";
-  }
   
-  public function format_user_function($depth, $function_name, $filename, $line, $elapsed_time, $memory_consumption) {
-    return sprintf("%s\t[%s:%d]", $function_name, $filename, $line);
-  }
-  
+  public $longest = 0;
+
   public function enterFunction($depth, $function_id, $elapsed_time, $memory_consumption, $function_name, $is_internal, $filename, $line, $included_from = NULL) {
     
+    /*
     $msg = $is_internal
       ? $this->format_internal_function( 
         $depth, $function_name, $elapsed_time, $memory_consumption)
       : $this->format_user_function( 
         $depth, $function_name, $filename, $line, $elapsed_time, $memory_consumption);
+    */
     
     $this->stack[$depth] = array(
       'name' => $function_name,
-      'msg' => $msg,
+      'file' => $is_internal ? '[INTERNAL]' : sprintf('[%s:%d]', $filename, $line),
     );
     
     if ($function_name == $this->watch_for_symbol) {
@@ -26,8 +23,11 @@ class WhoCallsStackTrace extends StackTraceForSymbol {
       $last = $this->stack[$depth -1];
       $last_func = $last['name'];
       
+      if (strlen($last['name']) > $this->longest) $this->longest = strlen($last['name']);
+      
       if (empty($this->traces[$last_func])) {
-        $this->traces[$last_func]['trace'] = $last['msg'];
+        $this->traces[$last_func]['name'] = $last['name'];
+        $this->traces[$last_func]['file'] = $last['file'];
         $this->traces[$last_func]['count'] = 1;
       }
       else {
